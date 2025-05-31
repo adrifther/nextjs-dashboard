1ba634f97e23e7b46ac0524d34ec22d2222173fa
adrifther <adrianferrert@gmail.com>
2025-05-31 14:51:29 +0200

    commit inicial

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets:['latin']});
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] }); 
+// how to make if bold 700 Lusitana font in the lusitana const by default if bold
+
