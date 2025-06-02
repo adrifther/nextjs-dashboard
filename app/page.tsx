cbbac3102ea60723ca47da44d940912b4396b2cb
adrifther <adrianferrert@gmail.com>
2025-06-02 08:36:45 +0200

    chapter 5 done

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,27 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
+      <Image
+        src='/hero-desktop.png'
+        width={1000}
+        height={760}
+        className="hidden md:block"
+        alt="Screenshots of the dashboardproject showing desktop version"
+      />
+      <Image
+        src='/hero-desktop.png'
+        width={560}
+        height={620}
+        className="hidden md:block"
+        alt="Screenshots of the dashboardproject showing mobile version"
+      />
       <div className={styles.shape}>
-        { <AcmeLogo /> }
+         <AcmeLogo /> 
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
